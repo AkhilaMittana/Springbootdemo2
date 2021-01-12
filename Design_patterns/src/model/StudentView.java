6289743749360af84f2bdd998dd93b630c5b3508
Akhila Mittana <Akhila Mittana@AkhilaMittana>
2021-01-12 17:38:49 +0530

    First commit.

---
+++ Design_patterns/src/model/StudentView.java
@@ -0,0 +1,11 @@
+package model;
+
+public class StudentView {
+	public void studentDetails(String name, int rollNo)
+	{
+		System.out.println("Student");
+		System.out.println("Name :"+name);
+		System.out.println("RollNo :"+rollNo);
+	}
+
+}
---
+++ Design_patterns/src/model/book/BookDao.java
@@ -0,0 +1,12 @@
+package model.book;
+import java.util.List;
+public interface BookDao {
+
+	    List<Books> getAllBooks();
+	    Books getBookByIsbn(int isbn);
+	    void saveBook(Books book);
+	    void deleteBook(Books book);
+	}
+
+
+
